1d6b5a67b4479d026302b2d68c803e8d5dea9a5e
Ambuj <ext.ambuj.kumar@stadiumgoods.com>
2022-08-16 20:19:56 +0530

    TDD dev continued

---
+++ foundry/test/QuestionnaireFactory.t.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+
+import "../src/QuestionnaireFactory.sol";
+
+contract QuestionnaireFactoryTest is DSTest {
+    QuestionnaireFactory public factory;
+
+    function setUp() public {
+        factory = new QuestionnaireFactory();
+    }
+
+    function testCreateQuestionnaire() public {
+        string memory question = "What is the final destination of ours at the job?";
+        string memory answer = "new guy";
+        bytes32 salt = bytes32("190190190"); 
+        bytes32 hashedAnswer = keccak256(abi.encodePacked(salt, answer));
+        factory.createQuestionnaire(question, hashedAnswer);
+        Questionnaire question_s = factory.questions(0);
+        assertEq(keccak256(abi.encodePacked(question_s.question())), keccak256(abi.encodePacked(question)));
+    }
+
+}
